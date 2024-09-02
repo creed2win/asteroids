df1e53f9b23c4ac61a125d7c91adfb3cb587a4e1
creed2win <michal.dovole@gmail.com>
2024-09-02 15:48:01 +0200

    definition of player

--- main.py
+++ main.py
@@ -7,7 +7,17 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
+    test = ['*', ' ', 'E', 'n', 'd', 'e', 'r', ' ', 'W', 'i', 'g', 'g', 'i', 'n']
+
+    joined_test = ''.join(test)
+
+    print(joined_test)
+
     pygame.init()
+
+    pyClock = pygame.time.Clock()
+    dt = 0
+
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
@@ -18,6 +28,8 @@ def main():
         
         screen.fill("#6e6e6e")
         pygame.display.flip()
+
+        dt = pyClock.tick(60)
 
 if __name__ == "__main__":
     main()
