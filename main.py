f98521091fd2992b11a7cb94b82234d512252166
creed2win <michal.dovole@gmail.com>
2024-09-04 09:14:35 +0200

    working draw player method

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import Player
 
 
 def main():
@@ -18,8 +19,9 @@ def main():
     pyClock = pygame.time.Clock()
     dt = 0
 
-
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    player = Player((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2))
 
     while True:
         for event in pygame.event.get():
@@ -27,10 +29,15 @@ def main():
                 return
         
         screen.fill("#6e6e6e")
+
+        player.draw(screen)
+        
         pygame.display.flip()
+        
 
         dt = pyClock.tick(60)
 
+        
 if __name__ == "__main__":
     main()
 
