986502359086bba294db28f9330367c28b8debcb
creed2win <michal.dovole@gmail.com>
2024-09-06 08:52:18 +0200

    working basic asteroid game

--- asteroid.py
+++ asteroid.py
@@ -13,3 +13,20 @@ class Asteroid(CircleShape):
 
     def update(self, dt):
         self.position += self.velocity * dt
+
+    def spawn(self, radius, position, velocity):
+        asteroid = Asteroid(position.x, position.y, radius)
+        asteroid.velocity = velocity
+
+    def split(self):
+        self.kill()
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
+        
+        rand_angle = random.uniform(20, 50)
+        
+        velocity1 = self.velocity.rotate(rand_angle)
+        velocity2 = self.velocity.rotate(-rand_angle)
+
+        asteroid1 = Asteroid.spawn(self, self.radius - ASTEROID_MIN_RADIUS, self.position, velocity1 * 1.2)
+        asteroid1 = Asteroid.spawn(self, self.radius - ASTEROID_MIN_RADIUS, self.position, velocity2 * 1.2)
